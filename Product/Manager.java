4c68fcb5c09f550b8c08d813b2898ea55326936c
Atsu <kamnknon@gmail.com>
2022-02-16 21:11:11 +0900

    第6章

---
+++ Product/Manager.java
@@ -0,0 +1,16 @@
+package Product;
+
+import java.util.*;
+
+public class Manager {
+  private HashMap<String, Product> showcase = new HashMap<>();
+
+  public void register(String name, Product proto) {
+    showcase.put(name, proto);
+  }
+
+  public Product create(String protoname) {
+    Product p = (Product) showcase.get(protoname);
+    return p.createClone();
+  }
+}
